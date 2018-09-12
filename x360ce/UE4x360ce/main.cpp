12504702fca55c593e2c604962ffa350d7bfb89d
Denys Dubinin <denys.dubinin@dimenco.eu>
2018-09-12 11:16:45 +0200

    add some c++

--- x360ce/UE4x360ce/main.cpp
+++ x360ce/UE4x360ce/main.cpp
@@ -2,6 +2,11 @@
 #include <thread>         // std::this_thread::sleep_for
 #include <chrono>         // std::chrono::seconds
 
+#include <windows.h>
+#include <Xinput.h>
+
+extern DWORD WINAPI XInputGetState(DWORD dwUserIndex, XINPUT_STATE* pState);
+
 int main()
 {
 	std::cout << "MAIN" << std::endl;
