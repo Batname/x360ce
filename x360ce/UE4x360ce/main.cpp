493d341a432e7627d205df978a7d81bf90d2564c
Batname <dadubinin@gmail.com>
2018-09-13 23:31:50 +0200

    clean c++ test code

--- x360ce/UE4x360ce/main.cpp
+++ x360ce/UE4x360ce/main.cpp
@@ -4,35 +4,28 @@
 
 #include <windows.h>
 #include <Xinput.h>
-#include <mutex>
 
 extern DWORD WINAPI XInputGetState(DWORD dwUserIndex, XINPUT_STATE* pState);
 extern "C" VOID WINAPI XInputEnable(BOOL enable);
 
-static std::mutex my_mutex;
-
 int main()
 {
-	std::cout << "MAIN" << std::endl;
 	DWORD dwUserIndex = 0;
 	XINPUT_STATE* pState = new XINPUT_STATE();
 
-	XInputEnable(true);
 	while (1)
 	{
-		my_mutex.lock();
 		XInputGetState(dwUserIndex, pState);
 		if (pState != nullptr)
 		{
-			std::cout << "GamePad" << pState->Gamepad.wButtons << std::endl;
+			std::cout << "GamePad >> " << pState->Gamepad.wButtons << std::endl;
 		}
 		else
 		{
 			std::cout << "No pState" << std::endl;
 		}
-		my_mutex.unlock();
 
-		//std::this_thread::sleep_for(std::chrono::milliseconds(50));
+		std::this_thread::sleep_for(std::chrono::milliseconds(200));
 	}
 
 	return 0;
