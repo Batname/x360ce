b4fd62322c64501d2539cab8233d4d69dbf50df2
Batname <dadubinin@gmail.com>
2018-09-14 09:43:59 +0200

    add comments

--- x360ce/UE4x360ce/main.cpp
+++ x360ce/UE4x360ce/main.cpp
@@ -12,6 +12,31 @@ int main()
 {
 	DWORD dwUserIndex = 0;
 	XINPUT_STATE* pState = new XINPUT_STATE();
+	//~~~~~~~~~~~~~~~~~~~ Update DLL form
+
+
+	//~~~~~~~~~~~~~~~~~~~ Loop throught all devices
+	// DirectInput in main app // C:\Users\denys.dubinin\AppData\Local\Temp\MetadataAsSource\01a98b802bf74db5b490cf94468ea36a\559446ddfdc948d5bc81210adf40afd9\DirectInput.cs
+	// NewDeviceForm if new device detected
+	// x360ce.App MainForm::RefreshCurrentInstances -> UpdateForm3 -> UpdateTimer_Elapsed
+	// DefaultPoolingInterval 50 ms
+	// DeviceInstance[] GetDevices()
+	// // Must find better way to find Device than by Vendor ID and Product ID.
+	//	var devs = DeviceDetector.GetDevices(classGuid, DIGCF.DIGCF_ALLCLASSES, null, j.Properties.VendorId, j.Properties.ProductId, 0);
+
+	// ~~~~~~~~~~~~~~~~~~ Try to find device config, othervice load from web
+	// forceRecountDevices = true by default, thet is why it load on the beginning and set false after first load try
+	// detector_DeviceChanged
+	// UpdateForm1() -> new DeviceDetector.DeviceDetectorEventHandler(detector_DeviceChanged);
+	// public void WndProc(ref Message m)
+	/**
+			/// <summary>
+		/// This function receives all the windows messages for this window (form).
+		/// We call the DeviceDetector from here so that is can pick up the messages about
+		/// drives arrived and removed.
+		/// </summary>
+		protected override void WndProc(ref Message m)
+	*/
 
 	while (1)
 	{
